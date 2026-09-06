ec3764ff3af4406a2b91eee96a1ec94a51fa9217
tagyne <tagyne@icloud.com>
2026-09-06 14:27:47 +0200

    fix: allow deleting non-empty ML bucket

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -2,7 +2,7 @@ resource "google_storage_bucket" "ml" {
   name                        = "student-performance-mlops-${var.project_id}"
   location                    = var.region
   uniform_bucket_level_access = true
-  force_destroy               = false
+  force_destroy               = true
   labels                      = local.labels
   versioning { enabled = true }
   depends_on = [google_project_service.required]
